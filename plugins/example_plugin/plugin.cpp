9c89bf2f01780944eb899606c1398d8fdb58a3bf
Samuel Bedoya <samuel.bedoyao@udea.edu.co>
2025-07-10 19:06:05 -0500

    Se creó un plugin de ejemplo ideado para la implementación de un modelo linear, donde el output de un plugin es el input del siguiente

---
+++ plugins/example_plugin/plugin.cpp
@@ -0,0 +1,40 @@
+#include "plugin_api.h"
+#include "state_vector_generated.h"
+#include <iostream>
+
+
+struct PluginState {
+    int tick_count = 0;
+};
+
+// --- Implementación de las funciones de la API ---
+
+PLUGIN_EXPORT PluginHandle plugin_create_instance() {
+    std::cout << "[Plugin] Creando instancia..." << std::endl;
+    PluginState* state = new PluginState();
+    return reinterpret_cast<PluginHandle>(state);
+}
+
+PLUGIN_EXPORT void plugin_destroy_instance(PluginHandle handle) {
+    std::cout << "[Plugin] Destruyendo instancia..." << std::endl;
+    PluginState* state = reinterpret_cast<PluginState*>(handle);
+    delete state;
+}
+
+PLUGIN_EXPORT uint32_t plugin_tick(PluginHandle handle,
+    uint8_t* buffer, uint32_t size) {
+
+    PluginState* state = reinterpret_cast<PluginState*>(handle);
+    state->tick_count++;
+    std::cout << "[Plugin] Tick #" << state->tick_count << " recibido." << std::endl;
+
+    auto mutable_state = state_vector::GetMutableVehicleState(buffer);
+
+    float current_x = mutable_state->position()->x();
+    std::cout << "[Plugin] Datos recibidos -> Posición X: " << current_x << std::endl;
+
+    mutable_state->mutable_position()->mutate_x(current_x + 1.0f);
+    std::cout << "[Plugin] Posición X modificada a: " << mutable_state->position()->x() << std::endl;
+
+    return 0;
+}
