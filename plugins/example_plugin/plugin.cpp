65aba49bed1fe044a4f5948dbbec5455c813cc0d
Samuel Bedoya <samuel.bedoyao@udea.edu.co>
2025-07-14 20:47:24 -0500

    Se añadieron comentarios a el plugin de ejemplo

--- plugins/example_plugin/plugin.cpp
+++ plugins/example_plugin/plugin.cpp
@@ -2,7 +2,7 @@
 #include "state_vector_generated.h"
 #include <iostream>
 
-
+// Como ejemplo se define un estado simple del plugin
 struct PluginState {
     int tick_count = 0;
 };
@@ -11,6 +11,7 @@ struct PluginState {
 
 PLUGIN_EXPORT PluginHandle plugin_create_instance() {
     std::cout << "[Plugin] Creando instancia..." << std::endl;
+    // El handle es un puntero a PluginState
     PluginState* state = new PluginState();
     return reinterpret_cast<PluginHandle>(state);
 }
@@ -18,6 +19,7 @@ PLUGIN_EXPORT PluginHandle plugin_create_instance() {
 PLUGIN_EXPORT void plugin_destroy_instance(PluginHandle handle) {
     std::cout << "[Plugin] Destruyendo instancia..." << std::endl;
     PluginState* state = reinterpret_cast<PluginState*>(handle);
+    // Aquí se liberan los recursos del plugin
     delete state;
 }
 
@@ -25,9 +27,11 @@ PLUGIN_EXPORT uint32_t plugin_tick(PluginHandle handle,
     uint8_t* buffer, uint32_t size) {
 
     PluginState* state = reinterpret_cast<PluginState*>(handle);
+    // Al avanzar el tick, se incrementa el contador del estado del plugin
     state->tick_count++;
     std::cout << "[Plugin] Tick #" << state->tick_count << " recibido." << std::endl;
 
+    // Consigue el estado del vehículo desde el buffer. Además, es modificable, lo que permite cambiar un item sin crear un nuevo buffer.
     auto mutable_state = state_vector::GetMutableVehicleState(buffer);
 
     float current_x = mutable_state->position()->x();
